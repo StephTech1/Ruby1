c8c3dc1e4e69a98b74b6faa3093c91b17b406291
Steph St <steph.streete@outlook.com>
2021-01-13 16:39:42 +0000

    added codes for HLT

--- main.rb
+++ main.rb
@@ -13,3 +13,14 @@ name = 'Nuriyah'
 age = 2 
 address = 'Arendelle'
 puts "My name is" + " " + name + " " + "I am" + " " + 2.to_s + " " + "Years old" " " + "I live in" + " " + address 
+#show current date and time
+require 'date'
+current_time = DateTime.now
+cdt = current_time.strftime "%d/%m/%Y %H:%M"
+puts "Current date and time:"+cdt 
+#Input first and last name and output in reverse order
+puts "Input your first name: "
+fname = gets.chomp
+puts "Input your last name "
+lname = gets.chomp
+puts "Hello #{lname} #{fname}"
