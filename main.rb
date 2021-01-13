ade5a6ba92771e446eafc944e42b99afc7bf7994
Steph St <steph.streete@outlook.com>
2021-01-13 16:09:28 +0000

    wrote more code

--- main.rb
+++ main.rb
@@ -1,11 +1,15 @@
+#print hello world
 puts "Hello World"
-
+#set variables
 name = 'Nuriyah'
 age = 2
 address = 'Arendelle'
-
+#print variables on sperate lines
 puts name
 puts age
 puts address
-
-puts name, age, address
+#concatenate all the info
+name = 'Nuriyah'
+age = 2 
+address = 'Arendelle'
+puts "My name is" + " " + name + " " + "I am" + " " + 2.to_s + " " + "Years old" " " + "I live in" + " " + address 
